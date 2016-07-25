8c8c7ffff89f3db7bac09492af79cdf2d9c73acf
JesusMtnez <jesusmartinez93@gmail.com>
2016-07-25 22:36:12 +0200

    [Python] Complete 'About Scoring Project' koans

--- jesusmtnez/python/koans/koans/about_scoring_project.py
+++ jesusmtnez/python/koans/koans/about_scoring_project.py
@@ -33,8 +33,22 @@ from runner.koan import *
 # Your goal is to write the score method.
 
 def score(dice):
-    # You need to write this method
-    pass
+    # Initial data
+    numbers = {}.fromkeys(range(1, 7), 0)
+    points = 0
+
+    # Fill dictionary with dice values
+    for i in dice:
+        numbers[i] += 1
+
+    # Algorithm
+    points += (numbers[1] // 3) * 1000
+    points += (numbers[1] % 3) * 100
+    points += (numbers[5] % 3) * 50
+    for n in range(2, 7):
+        points += (numbers[n] // 3) * 100 * n
+
+    return points
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
