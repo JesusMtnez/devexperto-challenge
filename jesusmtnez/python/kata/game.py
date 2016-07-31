6621a2105c2697a2df645a42129a945242dc2317
JesusMtnez <jesusmartinez93@gmail.com>
2016-07-31 19:27:45 +0200

    [Python] Fix game to support perfect game

--- jesusmtnez/python/kata/game.py
+++ jesusmtnez/python/kata/game.py
@@ -1,6 +1,6 @@
 class Game():
     def __init__(self):
-        self._rolls = [0] * 21
+        self._rolls = [0] * 23
         self._current_roll = 0
 
     def roll(self, pins):
@@ -11,7 +11,8 @@ class Game():
         score = 0
         for frame in range(0, 20, 2):
             if self._is_strike(frame):
-                score += 10 + self._rolls[frame + 2] + self._rolls[frame + 3]
+                score += 10 + self._rolls[frame + 2]
+                score += self._rolls[frame + 4] if self._is_strike(frame + 2) else self._rolls[frame + 3]
             elif self._is_spare(frame):
                 score += 10 + self._rolls[frame + 2]
             else:
@@ -22,7 +23,6 @@ class Game():
         return self._rolls[frame] + self._rolls[frame + 1] == 10
 
     def _is_strike(self, frame):
-        print(frame)
         return self._rolls[frame] == 10
 
     def _frame_score(self, frame):
