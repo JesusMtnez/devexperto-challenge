96eff716b13088cec1f238ed2f1803321aaced5c
JesusMtnez <jesusmartinez93@gmail.com>
2016-07-31 17:22:05 +0200

    Initial project

---
+++ jesusmtnez/python/kata/tests/test_game.py
@@ -0,0 +1,9 @@
+import unittest
+from game import Game
+
+class BowlingGameTest(unittest.TestCase):
+    def test_gutter_game(self):
+        g = Game()
+
+if __name__ == '__main__':
+    unittest.main()
