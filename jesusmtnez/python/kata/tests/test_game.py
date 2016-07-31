b5bf3a92309dac40deefbaea555e4d96aa6bca62
JesusMtnez <jesusmartinez93@gmail.com>
2016-07-31 17:58:27 +0200

    [Python] Refactor for code to roll many times

--- jesusmtnez/python/kata/tests/test_game.py
+++ jesusmtnez/python/kata/tests/test_game.py
@@ -9,16 +9,17 @@ class BowlingGameTest(unittest.TestCase):
     def tearDown(self):
         self.g = None
 
-    def test_gutter_game(self):
-        for i in range(20):
-            self.g.roll(0);
+    def _roll_many(self, n, pins):
+        "Roll 'n' times a roll of 'pins' pins"
+        for i in range(n):
+            self.g.roll(pins)
 
+    def test_gutter_game(self):
+        self._roll_many(20, 0)
         self.assertEqual(0, self.g.score())
 
     def test_all_ones(self):
-        for i in range(20):
-            self.g.roll(1)
-
+        self._roll_many(20, 1)
         self.assertEqual(20, self.g.score())
 
 if __name__ == '__main__':
