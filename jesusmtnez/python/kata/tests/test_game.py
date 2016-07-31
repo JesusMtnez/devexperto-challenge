48c765a99283cddf01273452df730b417217ab60
JesusMtnez <jesusmartinez93@gmail.com>
2016-07-31 17:45:53 +0200

    [Python] Refactor Game creation in tests

--- jesusmtnez/python/kata/tests/test_game.py
+++ jesusmtnez/python/kata/tests/test_game.py
@@ -2,20 +2,24 @@ import unittest
 from game import Game
 
 class BowlingGameTest(unittest.TestCase):
+
+    def setUp(self):
+        self.g = Game()
+
+    def tearDown(self):
+        self.g = None
+
     def test_gutter_game(self):
-        g = Game()
-
         for i in range(20):
-            g.roll(0);
+            self.g.roll(0);
 
-        self.assertEqual(0, g.score())
+        self.assertEqual(0, self.g.score())
 
     def test_all_ones(self):
-        g = Game()
         for i in range(20):
-            g.roll(1)
+            self.g.roll(1)
 
-        self.assertEqual(20, g.score())
+        self.assertEqual(20, self.g.score())
 
 if __name__ == '__main__':
     unittest.main()
